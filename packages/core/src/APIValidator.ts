fe7a080fddcf99e5fb97fb9251487528fbddb34f
Riccardo <riki.brero@gmail.com>
2022-03-02 22:04:39 +0100

    Update APIValidator.ts

--- packages/core/src/APIValidator.ts
+++ packages/core/src/APIValidator.ts
@@ -1,24 +1,58 @@
 import Ajv, { JTDSchemaType } from "ajv/dist/jtd";
 
+/**
+ * Function that processes the validated API request.
+ */
 export type APIFunction<RequestType, EnvironmentType> = {
   (request: RequestType, env: EnvironmentType): Promise<unknown>;
 };
 
 export interface APIEvent {
+  /**
+   * Optional body containing data.
+   *
+   * @default - none
+   */
   body?: string;
 }
 
 export interface APIResult {
+  /**
+   * HTTP response status code.
+   */
   statusCode: number;
+
+  /**
+   * Optional body containing data.
+   *
+   * @default - none
+   */
   body?: string;
 }
 
+/**
+ * @summary Data to initialize APIValidator.
+ */
 export interface APIValidatorData<RequestType, ResponseType, EnvironmentType> {
+  /**
+   * Schema used to validate the request.
+   */
   requestSchema: JTDSchemaType<RequestType>;
+
+  /**
+   * Schema used to validate the response.
+   */
   responseSchema: JTDSchemaType<ResponseType>;
+
+  /**
+   * Schema used to validate the environment variables.
+   */
   environmentSchema?: JTDSchemaType<EnvironmentType>;
 }
 
+/**
+ * @summary Validator of an API handler.
+ */
 export class APIValidator<RequestType, ResponseType, EnvironmentType> {
   private readonly data: APIValidatorData<RequestType, ResponseType, EnvironmentType>;
 
@@ -26,6 +60,15 @@ export class APIValidator<RequestType, ResponseType, EnvironmentType> {
     this.data = data;
   }
 
+  /**
+   * Performs the handler function and the validation of the request, of the response
+   * and optionally of the environment variables based on the provided schemas.
+   *
+   * @param event - API request event.
+   * @param handler - function that processes the validated request.
+   * @param environment - environment variables.
+   * @returns the result of the handler's execution.
+   */
   public async validate(
     event: APIEvent,
     handler: APIFunction<RequestType, EnvironmentType>,
@@ -34,6 +77,7 @@ export class APIValidator<RequestType, ResponseType, EnvironmentType> {
   ): Promise<APIResult> {
     const ajv = new Ajv();
 
+    // Validate environment variables.
     if (environment && this.data.environmentSchema) {
       const validateEnvironment = ajv.compile(this.data.environmentSchema);
 
@@ -45,10 +89,12 @@ export class APIValidator<RequestType, ResponseType, EnvironmentType> {
       }
     }
 
+    // Validate request.
     const request = JSON.parse(event.body ?? "{}");
     const validateRequest = ajv.compile(this.data.requestSchema);
 
     if (validateRequest(request)) {
+      // Validate response.
       const response = await handler(request, environment ?? {});
       const validateResponse = ajv.compile(this.data.responseSchema);
 
